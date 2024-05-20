87c6d97104b560b635884ce8f30eabbd933ecd8c
udp1024 <udp1024@hotmail.com>
2024-05-19 20:22:50 -0600

    wip - basic functionality exists. partial work on enhanced functionality

---
+++ docker-volume/start-linkarium.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## start-linkarium.sh ##
+# This script starts Linkarium container for DEV purposes
+
+# for Prod use the following command to start Linkarium
+# docker run -it --restart=unless-stopped -d \
+#     -p 10080:80 \
+#     -p 10443:443 \
+#     --name Linkarium \
+#     -v ~/proggy/Linkarium/docker-volume/web:/usr/share/nginx/html \
+#     -v ~/proggy/Linkarium/docker-volume/config/linkarium.conf:/etc/nginx/conf.d/linkarium.conf:ro \
+#     -v ~/proggy/Linkarium/docker-volume/ssl:/etc/nginx/certs/:ro
+#     nginx:1.25.5
+
+docker run -it --rm -d \
+    -p 10080:80 \
+    -p 10443:443 \
+    --name Linkarium \
+    -v ~/proggy/Linkarium/docker-volume/web:/usr/share/nginx/html \
+    -v ~/proggy/Linkarium/docker-volume/config/linkarium.conf:/etc/nginx/conf.d/linkarium.conf:ro \
+    -v ~/proggy/Linkarium/docker-volume/ssl:/etc/nginx/certs/:ro \
+    nginx:1.25.5
+    
--- docker-volume/web/js/bgscript.js
+++ docker-volume/web/js/bgscript.js
@@ -11,7 +11,7 @@ const imageUrls = [
 let currentIndex = Math.floor(Math.random() * imageUrls.length);
 
 function changeImage() {
-    imageContainer.src = imageUrls[currentIndex];
+    imageContainer.background = imageUrls[currentIndex];
     currentIndex = (currentIndex + 1) % imageUrls.length;
 }
 
@@ -20,3 +20,11 @@ changeImage();
 
 // Set interval to change image every 10 seconds
 setInterval(changeImage, 10000);
+
+
+/*<script type="text/javascript">
+
+var images = ['image-1.jpg', 'image-2.jpg', 'image-3.jpg', 'image-4.jpg'];
+document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+
+</script>*/
